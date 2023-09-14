eadd0843eeafb64723bcb43cb42e061eacb82625
Musab Abdullah <musababdullah6231@gmail.com>
2023-09-14 13:14:13 -0500

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Musab Abdullah on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
