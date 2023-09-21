4228e31984f8a0476a3f7777cbd68a85e9c2f663
Musab Abdullah <musababdullah6231@gmail.com>
2023-09-20 22:47:27 -0500

    got most of base functionality working. about panel isn't picking up icon from appIcon in assets for some reason though

---
+++ Focus/ApplicationMenu.swift
@@ -0,0 +1,59 @@
+//
+//  ApplicationMenu.swift
+//  Focus
+//
+//  Created by Musab Abdullah on 9/20/23.
+//
+
+import Foundation
+import SwiftUI
+
+class ApplicationMenu: NSObject {
+    let menu = NSMenu()
+    
+    func createMenu() -> NSMenu {
+        let focusView = FocusView()
+        let topView = NSHostingController(rootView: focusView)
+        topView.view.frame.size = CGSize(width: 300, height: 150)
+        
+        let customMenuItem = NSMenuItem()
+        customMenuItem.view = topView.view
+        menu.addItem(customMenuItem)
+        menu.addItem(NSMenuItem.separator())
+        
+        let aboutMenuItem = NSMenuItem(title: "About Focus Timer",
+                                       action: #selector(about),
+                                       keyEquivalent: "")
+        aboutMenuItem.target = self
+        menu.addItem(aboutMenuItem)
+        
+        let webLinkMenuItem = NSMenuItem(title: "Musab Abdullah",
+                                       action: #selector(openLink),
+                                       keyEquivalent: "")
+        webLinkMenuItem.target = self
+        webLinkMenuItem.representedObject = "https://musababdullah.webflow.io"
+        menu.addItem(webLinkMenuItem)
+        
+        let quitMenuItem = NSMenuItem(title: "Quit",
+                                       action: #selector(quit),
+                                       keyEquivalent: "q")
+        quitMenuItem.target = self
+        menu.addItem(quitMenuItem)
+        
+        return menu
+    }
+    
+    @objc func about(sender: NSMenuItem) {
+        NSApplication.shared.orderFrontStandardAboutPanel()
+    }
+    
+    @objc func openLink(sender: NSMenuItem) {
+        let link = sender.representedObject as! String
+        guard let url = URL(string: link) else { return }
+        NSWorkspace.shared.open(url)
+    }
+    
+    @objc func quit(sender: NSMenuItem) {
+        NSApp.terminate(self)
+    }
+}
---
+++ Focus/FocusView.swift
@@ -0,0 +1,170 @@
+//
+//  ContentView.swift
+//  Focus
+//
+//  Created by Musab Abdullah on 9/14/23.
+//
+
+import SwiftUI
+import AVFoundation
+
+
+enum TimerType: String, CaseIterable {
+    case pomodoro = "Pomodoro"
+    case shortbreak = "Short Break"
+    case longbreak = "Long Break"
+}
+
+/*
+ there are 3 different session
+ 1. 25 min focus
+ 2.  5 min break
+ 3. 10 min break
+ */
+class PomodoroModel: ObservableObject {
+    @Published var time = "25:00"
+    @Published var state = "idle"
+    @Published var buttonText = "start"
+    private var audioPlayer: AVAudioPlayer?
+    
+    // 0=pomodoro, 1=short_break, 2=long_break
+    @Published var timerType: TimerType = .pomodoro
+    
+    private var secondsLeft = 25*60
+    private var timer: Timer?
+    
+    init() {
+        self.time = "25:00"
+        self.state = "idle"
+        self.buttonText = "start"
+        self.secondsLeft = 25*60
+    }
+    
+    func start() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsLeft > 0 {
+                print("\(self.secondsLeft) sec left")
+                self.secondsLeft -= 1
+                self.time = self.timeText()
+            } else {
+                self.playSound()
+                Timer.invalidate()
+            }
+        }
+        if self.timerType == .pomodoro {
+            self.state = "focus"
+        } else if self.timerType == .shortbreak {
+            self.state = "break"
+        } else if self.timerType == .longbreak {
+            self.state = "chill"
+        }
+        
+        self.buttonText = "pause"
+    }
+    
+    func pause() {
+        guard let timer = self.timer else {return}
+        timer.invalidate()
+        self.state = "idle"
+        self.buttonText = "start"
+    }
+    
+    func statusText() -> String {
+        let messages = [
+            "focus": "time to work 🤓",
+            "idle": "lazy ahh boi 😡",
+            "break": "chill time 😎",
+            "chill": "touch some grass 🚶🏽‍♂️"
+        ]
+        if let status = messages[self.state] {
+            return status
+        }
+        return "wyd 🤨"
+    }
+    
+    func timeText() -> String {
+        return String(format: "%d:%02d", secondsLeft / 60, secondsLeft % 60)
+    }
+    
+    func setTimerType(type: TimerType) {
+        if let timer = self.timer {
+            timer.invalidate()
+            self.state = "idle"
+            self.buttonText = "start"
+        }
+        
+        if type == .pomodoro {
+            self.secondsLeft = 25*60
+        } else if type == .shortbreak {
+            self.secondsLeft = 5*60
+        } else if type == .longbreak {
+            self.secondsLeft = 10*60
+        }
+        self.time = self.timeText()
+    }
+    
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "goofy", ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            self.audioPlayer = try AVAudioPlayer(contentsOf: url)
+            self.audioPlayer?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
+
+struct FocusView: View {
+    @StateObject var model = PomodoroModel()
+    
+    var body: some View {
+        VStack {
+            Picker("", selection: $model.timerType) {
+                ForEach(TimerType.allCases, id: \.self) { option in
+                    Text(option.rawValue)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .onChange(of: model.timerType, perform: { (value) in
+                print("changed to \(value)")
+                model.setTimerType(type: value)
+            })
+            Text(model.time)
+                .font(.system(.largeTitle, design: .rounded))
+                .fontWeight(.black)
+            Text(model.statusText())
+                .font(.system(.title3, design: .rounded))
+            
+            Button{
+                print("clicked: \(model.buttonText)")
+                
+                if (model.state == "idle") {
+                    model.start()
+                } else {
+                    model.pause()
+                }
+            } label: {
+                Text(model.buttonText)
+                    .font(.system(.title3, design: .rounded))
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            
+        }
+        .padding()
+        .frame(width: 300, height: 150)
+        .background(Color.indigo)
+        
+    }
+}
+
+struct FocusView_Previews: PreviewProvider {
+    static var previews: some View {
+        FocusView()
+    }
+}
